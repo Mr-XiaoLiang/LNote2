5d6c2b97a529a9c6a4ada6a641a1cf932b8ebc86
lollipop <liujian594@hotmail.com>
2020-02-29 23:22:57 +0800

    主页布局搭建

--- app/src/main/java/com/lollipop/lnote/list/NoteCoverInfo.kt
+++ app/src/main/java/com/lollipop/lnote/list/NoteCoverInfo.kt
@@ -12,6 +12,39 @@ class NoteCoverInfo {
      */
     var labelColor = 0
 
+    /**
+     * 标题
+     */
+    var title = ""
 
+    /**
+     * 预览
+     */
+    var overview = ""
+
+    /**
+     * 包含支出
+     */
+    var isExpenditure = false
+
+    /**
+     * 包含收入
+     */
+    var isIncome = false
+
+    /**
+     * 包含提醒
+     */
+    var isAlert = false
+
+    /**
+     * 日期
+     */
+    var date = 0
+
+    /**
+     * 数据库id
+     */
+    var id = 0
 
 }
