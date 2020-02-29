33dd875c868c488e83c807e3bafff48001553824
lollipop <liujian594@hotmail.com>
2020-02-29 23:13:26 +0800

    主页布局搭建

---
+++ app/src/main/java/com/lollipop/lnote/info/LabelInfo.kt
@@ -0,0 +1,8 @@
+package com.lollipop.lnote.info
+
+/**
+ * @author lollipop
+ * @date 2020/2/29 23:07
+ * 标签信息
+ */
+data class LabelInfo(val name: String, val color: Int, val id: Int)
---
+++ app/src/main/java/com/lollipop/lnote/list/NoteCoverInfo.kt
@@ -0,0 +1,17 @@
+package com.lollipop.lnote.list
+
+/**
+ * @author lollipop
+ * @date 2020/2/29 23:01
+ * 笔记封面的描述信息
+ */
+class NoteCoverInfo {
+
+    /**
+     * 标签颜色
+     */
+    var labelColor = 0
+
+
+
+}
