ab7c794f9e1a85f6ee111fa311c2fe23507de33b
lollipop <liujian594@hotmail.com>
2020-03-09 00:32:48 +0800

    推进记事本开发进度

---
+++ app/src/main/java/com/lollipop/lnote/view/PlatterLayout.kt
@@ -0,0 +1,26 @@
+package com.lollipop.lnote.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+/**
+ * @author lollipop
+ * @date 2020/3/9 00:18
+ * 拼盘的Layout
+ */
+class PlatterLayout(context: Context, attrs: AttributeSet?, defStyleAttr: Int):
+    ViewGroup(context, attrs, defStyleAttr) {
+
+    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+    constructor(context: Context): this(context, null)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
